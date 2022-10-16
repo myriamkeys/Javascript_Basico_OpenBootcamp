ca661d2e3c3e9f2f2acc91838c700fd0943d7ab1
Miriam Cortes <miriamcortesluque@gmail.com>
2022-10-17 00:41:22 +0200

    npm Start

--- mi-primer-proyecto-de-node/index.js
+++ mi-primer-proyecto-de-node/index.js
@@ -1,9 +1,2 @@
 // Este es el mensaje de bienvenida
-console.log("Esta es la puerta de entrada al proyecto");
-
-/*
-Esta es la parte intermedia de nuestro proyecto
-Por último vamos a la última línea de saludo
-/Este va a ser el mensaje que se envía para despedir
-*/
-console.log("Adiós");
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
