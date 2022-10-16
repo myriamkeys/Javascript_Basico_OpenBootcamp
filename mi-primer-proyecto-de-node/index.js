97040533a6c896200c815b431d3a34e033b81820
Miriam Cortes <miriamcortesluque@gmail.com>
2022-10-17 00:26:07 +0200

    v1

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,9 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Esta es la parte intermedia de nuestro proyecto
+Por último vamos a la última línea de saludo
+/Este va a ser el mensaje que se envía para despedir
+*/
+console.log("Adiós");
